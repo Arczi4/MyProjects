e5eb573097618ee2bcbe686e3ae52ff83392d90d
Your Name <you@example.com>
2020-07-09 19:21:46 +0200

    Dodano warunek sprawdzajacy wygrana

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import numpy as np
+
 # create board
 board = ['0', '1', '2',
          '3', '4', '5',
          '6', '7', '8']
+
 player_flag = True
+game = True
 
 
 # board display
@@ -29,6 +33,35 @@ def move():
         player_flag = True
 
 
-while 1:
+def check_Win(bo, player):
+    if player:
+        le = 'X'
+    else:
+        le = 'O'
+
+    return not ( # check rows
+            (bo[0] == le and bo[1] == le and bo[2] == le) or
+            (bo[3] == le and bo[4] == le and bo[5] == le) or
+            (bo[6] == le and bo[7] == le and bo[8] == le) or
+
+            # check columns
+            (bo[0] == le and bo[3] == le and bo[6] == le) or
+            (bo[1] == le and bo[4] == le and bo[7] == le) or
+            (bo[3] == le and bo[5] == le and bo[8] == le) or
+
+            # check diagonals
+            (bo[0] == le and bo[4] == le and bo[8] == le) or
+            (bo[2] == le and bo[4] == le and bo[6] == le))
+
+
+
+while game:
+
     show_board()
     move()
+    game = check_Win(board, player_flag)
+    if not game:
+        if player_flag:
+            print("Wygrywa gracz X")
+        else:
+            print("Wygrywa gracz O")
