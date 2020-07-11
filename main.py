d626740e196b95f4c2adfb974656d28dbf021baa
Your Name <you@example.com>
2020-07-11 23:19:54 +0200

    Naprawiono błąd rozgrywki(player_flag), dodano interfejs

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ board = ['0', '1', '2',
 player_flag = True
 game = True
 
+# movement count
+m_count = 0
+
+def Clear_board():
+    global board
+    board = ['0', '1', '2',
+             '3', '4', '5',
+             '6', '7', '8']
 
 # board display
 def show_board():
@@ -20,10 +28,14 @@ def show_board():
 
 
 # player move
-# chceck player X(TRUE def) or O(FALSE)
-def move():
+# chceck player X(TRUE def)  O(FALSE)
+def move(board):
     global player_flag
+    global m_count
     pos = int(input('take a position 0-8'))
+    if not (pos>=0 and pos<10):
+        print("Wybrales złą pozycję! wybierz ponownie")
+        pos = int(input('Wybierz pozycje'))
     # player X
     if player_flag:
         board[pos] = 'X'
@@ -31,6 +43,8 @@ def move():
     else:
         board[pos] = 'O'
         player_flag = True
+    m_count += 1
+    return board
 
 
 def check_Win(bo, player):
@@ -39,7 +53,7 @@ def check_Win(bo, player):
     else:
         le = 'O'
 
-    return not ( # check rows
+    return (  # check rows
             (bo[0] == le and bo[1] == le and bo[2] == le) or
             (bo[3] == le and bo[4] == le and bo[5] == le) or
             (bo[6] == le and bo[7] == le and bo[8] == le) or
@@ -54,14 +68,56 @@ def check_Win(bo, player):
             (bo[2] == le and bo[4] == le and bo[6] == le))
 
 
+def is_full():
+    global m_count
+    if m_count <= 9:
+        return True
+    else:
+        print("Brak miejsca na tablicy.\nREMIS! ")
+        return False
 
-while game:
 
-    show_board()
-    move()
-    game = check_Win(board, player_flag)
-    if not game:
-        if player_flag:
-            print("Wygrywa gracz X")
-        else:
-            print("Wygrywa gracz O")
+def Game_1vs1():
+    while is_full:
+        check_Win_X = check_Win(board,player_flag)
+        check_Win_O = check_Win(board, not player_flag)
+        if check_Win_X or check_Win_O:
+            show_board()
+            if player_flag:
+                print("Wygrywa X!")
+
+            else:
+                print("Wygrywa O!")
+            print('Czy chcesz zagrać ponownie?')
+            x=int(input('1.Tak\n2.Nie'))
+            if x == 1:
+                Clear_board()
+                main()
+            else:
+                break
+
+        show_board()
+        move(board)
+
+def Game_vsComp():
+    pass
+
+def main():
+    print("Witaj w grze Tic Tac Toe\n"
+          "Wybierz 1. aby zagrać 1 na 1\n"
+          "Wybierz 2. aby zagrac z komputerem\n"
+          "Wybierz 3. aby wyjść z gry")
+    choice = int(input())
+    if choice == 1:
+        Game_1vs1()
+    elif choice == 2:
+        Game_vsComp()
+    elif choice == 3:
+        exit()
+
+
+main()
+
+
+
+
